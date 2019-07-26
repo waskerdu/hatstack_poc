9c4addae80d2bffc2567e61adc2fbdaa6eb53863
Riley Miller <rileywestonmiller@outlook.com>
2019-07-25 17:58:11 -0700

    added second poc with functions

--- hatstackpoc.py
+++ hatstackpoc.py
@@ -1,5 +1,5 @@
 # hatstack proof of concept
-import sys
+import sys, re
 
 class Token():
     def __init__(self):
@@ -7,16 +7,299 @@ class Token():
         self.position = 0
         self.file = ""
         self.type = ""
+        self.method = None
+        self.literal = ""
         # types include instruction, alias, label, literal
+        
+    def __init__(self, literal, line, _type, method, func = None):
+        self.line = line
+        self.position = 0
+        self.file = ""
+        self.type = _type
+        self.method = method
+        self.literal = literal
+        # types include instruction, alias, label, literal
+        
+    def __str__(self):
+        return self.literal + " line: " + str(self.line) + " type: " + self.type
 
 class Function():
     # sort of a mini interpreter
     def __init__(self):
+        self.name = ""
         self.ip = 0
         self.labels = {}
         self.mem = {}
         self.tokens = []
+        
+'''class Interpreter2():
+    
+    # methods with names starting with i are reserved for the interpreter's use
+        
+    def i_in(self):
+        self.stack.append( self.pop(self.inp) )
+        self.ip+=1
+        
+    def i_out(self):
+        self.out.append( self.stack.pop() )
+        self.ip+=1
+        
+    def i_add(self):
+        val = self.pop(self.stack) + self.pop(self.stack)
+        self.last_val = val
+        self.stack.append(val)
+        self.ip+=1
 
+    def i_sub(self):
+        val = self.pop(self.stack) - self.pop(self.stack)
+        self.last_val = val
+        self.stack.append(val)
+        self.ip+=1
+
+    def i_mul(self):
+        val = self.pop(self.stack) * self.pop(self.stack)
+        self.last_val = val
+        self.stack.append(val)
+        self.ip+=1
+
+    def i_div(self):
+        val = self.pop(self.stack) // self.pop(self.stack)
+        self.last_val = val
+        self.stack.append(val)
+        self.ip+=1
+
+    def i_mod(self):
+        val = self.pop(self.stack) % self.pop(self.stack)
+        self.last_val = val
+        self.stack.append(val)
+        self.ip+=1
+
+    def i_jump(self):
+        self.ip+=1
+        label = self.tokens[self.ip].literal
+        self.ip = self.labels[label]
+
+    def i_jez(self):
+        self.ip+=1
+        if self.pop(self.stack) != 0:
+            self.ip+=1
+            return
+        label = self.tokens[self.ip].literal
+        self.ip = self.labels[label]
+
+    def i_jlz(self):
+        self.ip+=1
+        if not self.pop(self.stack) < 0:
+            self.ip+=1
+            return
+        label = self.tokens[self.ip].literal
+        self.ip = self.labels[label]
+
+    def i_jgz(self):
+        self.ip+=1
+        if not self.pop(self.stack) > 0:
+            self.ip+=1
+            return
+        label = self.tokens[self.ip].literal
+        self.ip = self.labels[label]
+
+    def i_load(self):
+        self.ip+=1
+        alias = self.tokens[self.ip].literal
+        if alias not in self.mem:
+            self.mem[alias] = 0 # maybe should throw an error?
+        self.stack.append(self.mem[alias])
+        self.ip+=1
+        
+    def i_loadi(self):
+        self.ip+=1
+        alias = self.pop(self.stack)
+        if alias not in self.mem:
+            self.mem[alias] = 0
+        self.stack.append(self.mem[alias])
+        
+    def i_savei(self):
+        self.ip+=1
+        alias = self.pop(self.stack)
+        self.mem[alias] = self.pop(self.stack)
+
+    def i_save(self):
+        self.ip+=1
+        alias = self.tokens[self.ip].literal
+        self.ip+=1
+        self.mem[alias] = self.pop(self.stack)
+        
+    def i_push(self):
+        self.ip+=1
+        self.stack.append( int(self.tokens[self.ip].literal) )
+        self.ip+=1
+        
+    def i_dump(self):
+        self.dump()
+        self.ip+=1
+    
+    def i_del(self):
+        self.pop(self.stack)
+        self.ip+=1
+        
+    def i_exit(self):
+        self.exit = True
+        
+    def i_fun(self):
+        f = Function()
+        f.name = self.tokens[self.ip].literal
+        self.ip+=1
+        while self.ip < len(self.tokens):
+            #print(self.ip)
+            token = self.tokens[self.ip]
+            if token.method == self.i_endfun:
+                break
+            if token.method == self.i_fun:
+                
+            f.tokens.append(token)
+            self.functions[f.name]=f
+            self.ip+=1
+        self.function_stack.append(f)
+        
+    def i_endfun(self):
+        self.ip+=1
+        
+    def i_call(self):
+        self.ip+=1
+        
+    def i_noop(self):
+        self.ip+=1
+        
+    def __init__(self):
+        self.tokens = []
+        self.inp = []
+        self.out = []
+        self.validate = []
+        self.stack = []
+        self.labels = {}
+        self.mem = {}
+        self.last_val = None
+        self.ip = 0
+        self.exit = False
+        self.functions = {}
+        self.function_stack = []
+        self.instructions = {
+                "in": self.i_in,
+                "add": self.i_add,
+                "sub": self.i_sub,
+                "mul": self.i_mul,
+                "div": self.i_div,
+                "mod": self.i_mod,
+                "out": self.i_out,
+                "jump": self.i_jump,
+                "jez": self.i_jez,
+                "jlz": self.i_jlz,
+                "jgz": self.i_jgz,
+                "save": self.i_save,
+                "savei": self.i_savei,
+                "load": self.i_load,
+                "loadi": self.i_loadi,
+                "dump": self.i_dump,
+                "push": self.i_push,
+                "del": self.i_del,
+                "exit": self.i_exit,
+                "endfun": self.i_endfun,
+                }
+    
+    def pop(self, source):
+        if len(source) > 0:
+            return source.pop()
+        self.exit = True
+        return 0
+    
+    def is_number(self, string):
+        match = re.search("[0-9]",string)
+        if match:
+            if match.span()[0] == 0:
+                return True
+        return False
+    
+    def tokenize(self, tokens, line):
+        for token in tokens:
+            t = None
+            if token in self.instructions:
+                t = Token(token, line, "instruction", self.instructions[token])
+            elif token[0] == "#":
+                self.labels[token[1:]] = len(self.tokens)
+                continue
+            elif token[0] == "@":
+                self.functions[token[1:]] = None
+                t = Token(token[1:], line, "function", self.i_fun)
+            elif self.is_number(token):
+                t = Token(token, line, "literal", self.i_noop)
+            elif token in self.functions:
+                t = Token(token, line, "call", self.i_call)
+            else:
+                t = Token(token, line, "alias", self.i_noop)
+            self.tokens.append(t)
+
+    def load_program(self, program):
+        if program.find(".hat") != -1:
+            f = open(program)
+            line_num = 0
+            for line in f.readlines():
+                comment = line.find("//")
+                if comment != -1:
+                    line = line[:comment]
+                line = line.lower().split()
+                self.tokenize(line, line_num)
+                line_num+=1
+            f.close()
+        
+    def load_problem(self, problem = None):
+        #self.inp = [3, 9, 10, 5]
+        #self.validate = [15, 12]
+        #self.inp = [9,7,20,13]
+        self.inp = [0,9,12,7,24]
+        self.validate = [1,1]
+        
+    def run(self):
+        self.ip = 0
+        #while self.ip < 4:
+            #print(len(self.tokens))
+        while self.ip < len(self.tokens):
+            #print(self.ip)
+            if self.exit:
+                break
+            token = self.tokens[self.ip]
+            print(token.literal)
+            token.method()
+            #self.instructions[token]()
+            
+    def run_empty(self):
+        for i in range(3):
+            self.run()
+            if len(self.inp) == 0 or self.exit:
+                break
+            
+    def is_valid(self):
+        if str(self.validate) == str(self.out):
+            print("Program successfully validated!")
+        else:
+            print("Output was incorrect.")
+            self.dump()
+            
+    def dump(self):
+        print("in:", self.inp)
+        print("out:", self.out)
+        print("expected:", self.validate)
+        print("stack:", self.stack)
+        print("lables:", self.labels)
+        print("aliases:", self.mem)
+        print("functions:", self.functions)
+        print("ip:", self.ip)
+        print("last value", self.last_val)
+        
+    def dump_tokens(self):
+        for token in self.tokens:
+            print(token)
+
+'''
 class Interpreter():
     
     # methods with names starting with i are reserved for the interpreter's use
@@ -190,8 +473,8 @@ class Interpreter():
     def load_problem(self, problem = None):
         #self.inp = [3, 9, 10, 5]
         #self.validate = [15, 12]
-        #self.inp = [9,7,20,13]
-        self.inp = [0,9,12,7,24]
+        self.inp = [9,7]
+        #self.inp = [0,9,12,7,24]
         self.validate = [1,1]
         
     def run(self):
@@ -231,8 +514,10 @@ class Interpreter():
 if __name__ == "__main__":
     program = ""
     if len(sys.argv) == 1:
+        #program = "sample.hat"
         program = "insert.hat"
         #program = "fib.hat"
+        #program = "functiontest.hat"
     else:
         program = sys.argv[1]
     interp = Interpreter()
@@ -242,4 +527,4 @@ if __name__ == "__main__":
     #print(interp.tokens)
     interp.run()
     #interp.is_valid()
-    #interp.dump()
+    interp.dump()
---
+++ hatstackpoc2.py
@@ -0,0 +1,352 @@
+# hatstack v 2
+import sys, re, json
+
+class Token():
+    def __init__(self):
+        self.val = ""
+        self.type = ""
+        self.method = None
+        self.line = 0
+        
+    def __str__(self):
+        out = ["value:", self.val, "type:", self.type, "line:", str(self.line)]
+        return " ".join(out)
+    
+class Function():
+    
+    def throw_error(self, message):
+        if not self.error:
+            self.error = True
+            print(message)
+            
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            self.throw_error("Stack empty, nothing to pop!")
+            return 0
+        
+    def jump(self):
+        self.ip+=1
+        t = self.tokens[self.ip]
+        #print("jump:",t.val)
+        if t.val in self.labels:
+            self.ip = self.labels[t.val]
+        else:
+            self.throw_error("'"+t.val+"' is an invalid label")
+        
+    def i_load(self):
+        self.ip+=1
+        alias = self.tokens[self.ip].val
+        if alias not in self.aliases:
+            self.aliases[alias] = 0
+            #self.throw_error("'"+alias+"'  is not a recognized variable.")
+            # add more checks for scope and so on
+            self.ip+=1
+            return
+        self.stack.append(self.aliases[alias])
+        self.ip+=1
+        
+    def i_loadi(self):
+        self.ip+=1
+        alias = self.pop()
+        if alias not in self.aliases:
+            self.aliases[alias] = 0
+            # add more checks for scope and so on
+        self.stack.append(self.aliases[alias])
+        
+    def i_save(self):
+        self.ip+=1
+        alias = self.tokens[self.ip].val
+        self.ip+=1
+        self.aliases[alias] = self.pop()
+        
+    def i_savei(self):
+        self.ip+=1
+        alias = self.pop()
+        self.aliases[alias] = self.pop()
+        
+    def i_push(self):
+        self.ip+=1
+        self.stack.append( int(self.tokens[self.ip].val) )
+        self.ip+=1
+        
+    def i_add(self):
+        self.stack.append( self.pop() + self.pop() )
+        self.ip+=1
+        
+    def i_sub(self):
+        val = self.pop() - self.pop()
+        self.stack.append( val )
+        self.ip+=1
+        
+    def i_mul(self):
+        self.stack.append( self.pop() * self.pop() )
+        self.ip+=1
+        
+    def i_div(self):
+        self.stack.append( self.pop() // self.pop() )
+        self.ip+=1
+        
+    def i_mod(self):
+        self.stack.append( self.pop() % self.pop() )
+        self.ip+=1
+        
+    def i_jump(self):
+        self.jump()
+        
+    def i_jgz(self):
+        if self.pop() > 0:
+            self.jump()
+        else:
+            self.ip+=2
+            
+    def i_jlz(self):
+        if self.pop() < 0:
+            self.jump()
+        else:
+            self.ip+=2
+            
+    def i_jez(self):
+        if self.pop() == 0:
+            self.jump()
+        else:
+            self.ip+=2
+            
+    def i_jnz(self):
+        if self.pop() != 0:
+            self.jump()
+        else:
+            self.ip+=2
+        
+    def i_call(self):
+        None
+        
+    def i_endfunc(self):
+        None
+        
+    def i_out(self):
+        self.outbox.append(self.pop())
+        self.ip+=1
+        
+    def i_in(self):
+        if len(self.inbox) > 0:
+            val = self.inbox.pop()
+            #print(val, self)
+            self.stack.append( val )
+        else:
+            self.throw_error("Inbox empty, nothing to pop!")
+        self.ip+=1
+        
+    def i_dump(self):
+        print(self)
+        self.ip+=1
+        
+    instructions = {
+            "push": i_push,
+            "out": i_out,
+            "in": i_in,
+            "save": i_save,
+            "savei": i_savei,
+            "load": i_load,
+            "loadi": i_loadi,
+            "add": i_add,
+            "sub": i_sub,
+            "mul": i_mul,
+            "div": i_div,
+            "mod": i_mod,
+            "jump": i_jump,
+            "jgz": i_jgz,
+            "jlz": i_jlz,
+            "jez": i_jez,
+            "jnz": i_jnz,
+            "dump": i_dump,
+            "endfunc": i_endfunc,
+            }
+    
+    def parse(self, tokens, start):
+        tokens = tokens[:]
+        self.tokens.clear()
+        #print("parse", start)
+        aliases = []
+        i = 0
+        while i + start < len(tokens):
+            t = tokens[i + start]
+            if t.val == "endfunc":
+                i+=2
+                break
+            if t.type == "label":
+                self.labels[t.val] = i
+            elif t.type == "function":
+                f = Function(t.val, self.stack, self.inbox, self.outbox)
+                self.functions[t.val] = f
+                i += f.parse(tokens, i+start+1)
+                continue
+            elif t.type == "alias":
+                aliases.append(t)
+                
+            i+=1
+            #print(t)
+            self.tokens.append(t)
+        for alias in aliases:
+            if alias.val in self.functions:
+                alias.type = "function_tag"
+                alias.method= self.i_call
+            elif alias.val in self.labels:
+                #print("found",alias.val)
+                alias.type = "label_tag"
+            
+        #print("endparse")
+        return i
+        
+    def __init__(self, name, stack, inbox, outbox):
+        self.name = name
+        self.tokens = []
+        self.inbox = inbox
+        self.outbox = outbox
+        self.ip = 0
+        self.stack = stack
+        self.aliases = {}
+        self.labels = {}
+        self.functions = {}
+        self.error = False
+        self.parent = None
+        
+    def __str__(self):
+        out = ["name: ", self.name, "ip:", str(self.ip), "stack:", str(self.stack)]
+        #out = ["inbox:", str(self.inbox), "outbox:", str(self.outbox) "stack: ", str(self.stack)]
+        #out += ["aliases", str(self.aliases)]
+        return " ".join(out)
+    
+    def dump_tokens(self):
+        i = 0
+        for token in self.tokens:
+            print(token)
+            print(i)
+            i+=1
+        
+    def run(self):
+        self.ip = 0
+        #print("running:",self.name)
+        
+        while self.ip < len(self.tokens):
+            t = self.tokens[self.ip]
+            #print(t)
+            if t.type == "instruction":
+                #print(t)
+                t.method(self)
+            elif t.type == "label":
+                self.ip+=1
+            elif t.type == "function_tag":
+                self.functions[t.val].run()
+                self.ip+=1
+            else:
+                self.throw_error("'"+t.val+"' on line "+ str(t.line+1) + " is not an instruction")
+            if self.error:
+                break
+        #print(self.ip)
+        #print(self)
+        return self.ip
+    
+class Interpreter(Function):
+    def __init__(self):
+        self.name = "interpreter"
+        self.inbox = []
+        self.outbox = []
+        self.valid_outbox = []
+        self.tokens = []
+        self.ip = 0
+        self.stack = []
+        self.aliases = {}
+        self.labels = {}
+        self.functions = {}
+        self.error = False
+        self.problem = None
+        self.program = None
+        
+    def __str__(self):
+        out = ["inbox:", str(self.inbox), "outbox:", str(self.outbox), "valid:", str(self.valid_outbox), "stack: ", str(self.stack)]
+        out += ["aliases", str(self.aliases)]
+        out += ["labels", str(self.labels)]
+        out += ["ip", str(self.ip)]
+        return " ".join(out)
+        
+    def is_number(self, string):
+        match = re.search("[0-9]",string)
+        if match:
+            if match.span()[0] == 0:
+                return True
+        return False
+        
+    def tokenize(self, tokens, line_num):
+        #print(tokens)
+        for token in tokens:
+            t = Token()
+            t.val = token
+            t.line = line_num
+            if token in self.instructions:
+                t.type = "instruction"
+                t.method = self.instructions[token]
+            elif token[0]=="#":
+                t.val = token[1:]
+                t.type = "label"
+            elif token[0]=="@":
+                t.val = token[1:]
+                t.type = "function"
+            elif self.is_number(token):
+                t.type = "literal"
+            else:
+                t.type = "alias"
+            self.tokens.append(t)
+        
+    def create_problem(self, filename, inbox, valid_outbox):
+        problem = {
+                "inbox": inbox,
+                "valid_outbox": valid_outbox
+                }
+        f = open(filename, "w")
+        f.write(json.dumps(problem))
+        f.close()
+        
+    def load_problem(self, filename):
+        f = open(filename)
+        problem = json.loads(f.read())
+        self.inbox = problem["inbox"]
+        self.valid_outbox = problem["valid_outbox"]
+        f.close()
+        
+    def load_program(self, filename):
+        f = open(filename)
+        line_num = 0
+        for line in f.readlines():
+            comment = line.find("//")
+            if comment != -1:
+                line = line[:comment]
+            line = line.lower().split()
+            if len(line) == 0:
+                line_num+=1
+                continue
+            self.tokenize(line, line_num)
+            line_num+=1
+        f.close()
+        self.parse(self.tokens, 0)
+        
+    def validate(self):
+        if str(self.outbox) == str(self.valid_outbox):
+            print("Solution successfully validated!")
+            return True
+        else:
+            print("Output is not correct.")
+            return False
+        
+interp = Interpreter()
+interp.load_problem("insert.prob")
+interp.load_program("insert.hat")
+#interp.load_program("functiontest.hat")
+#print(interp)
+#interp.dump_tokens()
+interp.run()
+print(interp)
+#interp.validate()
+        
+#interpreter.create_problem("fib.prob", [9], [1,1,2,3,5,8])
